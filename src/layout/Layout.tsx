618154fe0b2fab7a17c11b011e042b9ad943b7f4
Károly Török <torok.karoly.krisztian@gmail.com>
2024-02-29 15:43:56 +0100

    Absolute imports

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import "@picocss/pico/css/pico.orange.min.css";
 import type { FC, PropsWithChildren } from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
